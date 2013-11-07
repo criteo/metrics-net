e9ce62d8aea06d626170ca9a54b4ab2b10d8b722
Christophe Juniet <c.juniet@criteo.com>
2013-11-07 11:30:19 +0100

    Support.Random can also use fast PRNG
    Enable the use of a fast PRNG instead of a strong cryptographic random
    number generator.

--- src/metrics/Support/Random.cs
+++ src/metrics/Support/Random.cs
@@ -6,21 +6,37 @@ namespace metrics.Support
     /// <summary>
     /// Provides statistically relevant random number generation
     /// </summary>
-    internal class Random
+    public class Random
     {
         private static readonly RandomNumberGenerator _random;
+        private static readonly System.Random _prng;
+
+        public static bool UseCrypto { get; set; }
 
         static Random()
         {
-            _random = RandomNumberGenerator.Create();   
+            _random = RandomNumberGenerator.Create();
+            _prng = new System.Random();
+            UseCrypto = true;
         }
-        
+
         public static long NextLong()
+        {
+            return (UseCrypto ? NextLongCrypto() : NextLongFast());
+        }
+
+        public static long NextLongCrypto()
         {
             var buffer = new byte[sizeof(long)];
             _random.GetBytes(buffer);
-            var value = BitConverter.ToInt64(buffer, 0);
-            return value;
+            return BitConverter.ToInt64(buffer, 0);
+        }
+
+        public static long NextLongFast()
+        {
+            var buffer = new byte[sizeof(long)];
+            _prng.NextBytes(buffer);
+            return BitConverter.ToInt64(buffer, 0);
         }
     }
 }
