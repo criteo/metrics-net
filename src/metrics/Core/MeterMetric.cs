de861536a9517d3b050535f6b38d2f1a2e29affa
Antoine Souques <a.souques@criteo.com>
2014-06-25 10:49:26 +0200

    Add Clear method in MeterMetric
    Used to set count and rates at 0.

--- src/metrics/Core/MeterMetric.cs
+++ src/metrics/Core/MeterMetric.cs
@@ -45,7 +45,20 @@ namespace metrics.Core
         {
             EventType = eventType;
             RateUnit = rateUnit;
-            _timer = new Timer(_ => Tick(), null, TimeSpan.Zero, Interval);
+            _timer = new Timer(_ => Tick(), null, Interval, Interval);
+        }
+
+        /// <summary>
+        /// Clears all recorded values
+        /// </summary>
+        public void Clear()
+        {
+            _count.Set(0);
+            _m1Rate.Clear();
+            _m5Rate.Clear();
+            _m15Rate.Clear();
+            _startTime = DateTime.UtcNow.Ticks;
+            _timer.Change(Interval, Interval);
         }
 
         /// <summary>
