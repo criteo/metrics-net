8b5d8b2a4e7f3f8769ec47e93c79fd1e9a49a0ac
Guillaume Turri <g.turri@criteo.com>
2016-02-11 17:32:46 +0100

    Fix UTests which relied on sleep, on Ubuntu/dotnet-core

--- src/metrics.Tests/Core/MeterTests.cs
+++ src/metrics.Tests/Core/MeterTests.cs
@@ -36,7 +36,7 @@ namespace metrics.Tests.Core
                     meter.Mark();
                     i++;
                 }
-                Thread.Sleep(5000); // Wait for at least one EWMA rate tick
+                Thread.Sleep(50000); // Wait for at least one EWMA rate tick
                 block.Set();
             });
             block.WaitOne();
