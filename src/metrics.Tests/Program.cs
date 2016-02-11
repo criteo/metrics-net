d43b7d6b3c72457dfa2d9a3548ded42563c2d643
Nicolas Truchi <n.truchi@criteo.com>
2016-02-11 16:47:09 +0100

    Add Entry point for NUnitLite test runner

---
+++ src/metrics.Tests/Program.cs
@@ -0,0 +1,16 @@
+﻿#if COREFX
+using NUnitLite;
+using System;
+using System.Reflection;
+
+namespace metrics.Tests
+{
+    public class Program
+    {
+        public int Main(string[] args)
+        {
+            return new AutoRun().Execute(typeof(Program).GetTypeInfo().Assembly, Console.Out, Console.In, args);
+        }
+    }
+}
+#endif
